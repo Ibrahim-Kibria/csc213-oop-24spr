3f9065c659414038977e2f243facf2179025fa32
ibrahim kibria <1611048@iub.edu.bd>
2024-02-02 23:17:41 +0600

    first commit

---
+++ class projects/Arithmatic Operation/src/ArithmaticOperationSceneController.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author god_bhai
+ */
+public class ArithmaticOperationSceneController implements Initializable {
+
+    @FXML
+    private TextField xTextField;
+    @FXML
+    private TextField yTextField;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void addButtonClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void subButtonClicked(MouseEvent event) {
+    }
+
+    @FXML
+    private void mulButtonClicked(MouseEvent event) {
+    }
+    
+}
